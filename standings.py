1f2b329e8e9dece9d3b72befd5d89cfcd07ee482
Ankith Kodali <ankith.kodali@gmail.com>
2024-01-25 15:57:32 -0500

    Update standings.py for chocks name

--- standings.py
+++ standings.py
@@ -78,7 +78,7 @@ def main():
 
     # manual data import cause rish screwed it all up
     data = {
-        "Team": ["Thunderstruck", "White Lightning", "Chef Curry", "Mark Will didnt make it", "TJ's Tall Boys", "Bucksketball", "LameloBawl", "Bum Squad", "Tel Aviv Basketball Club", "Team Srivastava"],
+        "Team": ["Thunderstruck", "White Lightning", "Ant-Man and the BI", "Mark Will didnt make it", "TJ's Tall Boys", "Bucksketball", "LameloBawl", "Bum Squad", "Tel Aviv Basketball Club", "Team Srivastava"],
         "Record": ["10-3", "10-3", "8-5", "6-7", "7-6", "7-6", "6-7", "4-9", "5-8", "2-11"]
     }
     df_new = pd.DataFrame(data)
