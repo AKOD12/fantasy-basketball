27b2701674a8f3940141cf23115bb62c06a00a4f
Ankith Kodali <ankith.kodali@gmail.com>
2024-02-05 00:53:22 -0500

    Update standings.py

--- standings.py
+++ standings.py
@@ -79,7 +79,7 @@ def main():
     # manual data import cause rish screwed it all up
     data = {
         "Team": ["Thunderstruck", "White Lightning", "Ant-Man and the BI", "Mark Will didnt make it", "TJ's Tall Boys", "Bucksketball", "LameloBawl", "Bum Squad", "Tel Aviv Basketball Club", "Team Srivastava"],
-        "Record": ["11-3", "11-3", "8-6", "7-7", "7-7", "8-6", "6-8", "4-10", "6-8", "2-12"]
+        "Record": ["12-3", "11-4", "8-7", "7-8", "7-8", "9-6", "7-8", "5-10", "6-9", "3-12"]
     }
     df_new = pd.DataFrame(data)
     current_date = datetime.now(pytz.timezone('US/Eastern'))
